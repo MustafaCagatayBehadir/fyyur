d82da2ee7436bc73f12561b07d39403ac9494d37
mbehadir <mbehadir@cisco.com>
2022-01-26 22:33:48 +0300

    env variables are added.

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -1,9 +1,6 @@
 from crypt import methods
-from importlib import resources
-import os
 import sys
 from flask import Flask, request, abort, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import random
 from models import setup_db, Question, Category, db
